988b90fe1c4cf61f204ddf7dc01e2d995320bcba
krennarjashari <62670924+krennarjashari@users.noreply.github.com>
2023-03-09 09:42:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
         <Die value="4" />
         <Die value="5" />
         <Die value="6" />
-        <Die value="7" />
-        <Die value="8" />
-        <Die value="9" />
-        <Die value="10" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="2" />
       </div>
     </main>
   );
