388496defe26c2a5fc8d452988d90fd2b358443f
krennarjashari <62670924+krennarjashari@users.noreply.github.com>
2023-03-09 10:41:05 +0100

    roll dice css features

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import Die from './components/Die';
+import React from 'react';
 
 function App() {
+
+  const [dice, setDice] = React.useState(allnewDice())
+
+  function allnewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+  }
+
+  function rollDice() {
+    setDice(allnewDice())
+  }
+
+  const diceElements = dice.map(die => <Die value={die} />)
+
   return (
-    <main>
+    <main className="main-container">
       <div className="dice-container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="2" />
+        {diceElements}
       </div>
+      <button onClick={rollDice} className="roll-dice">Roll Dice</button>
     </main>
   );
 }
