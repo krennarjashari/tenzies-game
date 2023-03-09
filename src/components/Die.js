d037a1db8ca23ebda003107ebeca4dd1b17d0d1c
krennarjashari <62670924+krennarjashari@users.noreply.github.com>
2023-03-09 09:41:07 +0100

    code refactor

--- src/components/Die.js
+++ src/components/Die.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Die(props){
     return(
-        <div>
-            <h2>{props.value}</h2>
+        <div className="die-face">
+            <h2 className="die-num">{props.value}</h2>
         </div>
     )
 }
